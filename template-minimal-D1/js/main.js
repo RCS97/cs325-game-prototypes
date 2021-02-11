f1cd40873be690ef5f1993923f137a4ecbfab279
RCS97 <63381610+RCS97@users.noreply.github.com>
2021-02-11 00:31:01 -0500

    D1 game
    Digital assignment game code

--- template-minimal-D1/js/main.js
+++ template-minimal-D1/js/main.js
@@ -1,57 +1,117 @@
+
+/*
+ * CS 325 - Digital Assignment 1
+ *
+ */
+
 import "./phaser.js";
 
-// You can copy-and-paste the code from any of the examples at https://examples.phaser.io here.
-// You will need to change the `parent` parameter passed to `new Phaser.Game()` from
-// `phaser-example` to `game`, which is the id of the HTML element where we
-// want the game to go.
-// The assets (and code) can be found at: https://github.com/photonstorm/phaser3-examples
-// You will need to change the paths you pass to `this.load.image()` or any other
-// loading functions to reflect where you are putting the assets.
-// All loading functions will typically all be found inside `preload()`.
+var background;
+var isRunning = 0;
+var circlesClicked = 0;
+var time;
+var score;
 
-// The simplest class example: https://phaser.io/examples/v3/view/scenes/scene-from-es6-class
+
 
 class MyScene extends Phaser.Scene {
-    
+
     constructor() {
         super();
-        
-        this.bouncy = null;
     }
     
     preload() {
-        // Load an image and call it 'logo'.
-        this.load.image( 'logo', 'assets/phaser.png' );
+		this.load.image( 'background', 'assets/starfield.jpg' );
     }
     
     create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        this.bouncy = this.physics.add.sprite( this.cameras.main.centerX, this.cameras.main.centerX, 'logo' );
-        
-        // Make it bounce off of the world bounds.
-        this.bouncy.body.collideWorldBounds = true;
-        
-        // Make the camera shake when clicking/tapping on it.
-        this.bouncy.setInteractive();
-        this.bouncy.on( 'pointerdown', function( pointer ) {
-            this.scene.cameras.main.shake(500);
-            });
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        let style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        let text = this.add.text( this.cameras.main.centerX, 15, "Build something amazing.", style );
-        text.setOrigin( 0.5, 0.0 );
+		// add background
+		var windowWidth = window.innerWidth;
+		var windowHeight = window.innerHeight;
+		this.bg = this.add.image(windowWidth / 2, windowHeight / 2, 'background');
+		this.bg.setDisplaySize(windowWidth, windowHeight);
+		
+
+		// UI bar
+		this.add.rectangle(400, 600, 800, 100, 0x000);
+
+		let textStyle = { font: "20px Verdana", fill: "#FFF", background: "#435543"};
+		const startBut = this.add.text(50, 560, 'Start', textStyle)
+			.setInteractive()
+			.on('pointerdown', () => this.startButton() );
+		const stopBut = this.add.text(150, 560, 'Stop', textStyle)
+			.setInteractive()
+			.on('pointerdown', () => this.stopButton() );
+
+		time = this.add.text(250, 560, "0", textStyle);
+		score = this.add.text(350, 560, "0", textStyle);
     }
     
     update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        this.bouncy.rotation = this.physics.accelerateToObject( this.bouncy, this.input.activePointer, 500, 500, 500 );
+		var curTime = parseInt(time.text.toString());
+		//console.log(curTime);
+
+		var x = Math.floor(Math.random()*800);
+		var y = Math.floor(Math.random()*500);
+
+		// check if running
+		if(isRunning) {
+			// check if below max time
+			if(curTime<1500) {
+				// increment time
+				time.setText(curTime+1);
+
+				if(curTime%20==0)
+					// add circle at certain times
+					var circle = this.add.circle(x, y, 50, get_hex_color())
+						.setInteractive()
+						.on('pointerdown', () => this.circleClick(circle));
+			}
+			else {
+				// max time reached
+				isRunning = 0;
+			}
+		}
+
     }
+
+	// reset scene, then start timer and set to running
+	startButton() {
+		this.scene.restart();
+		console.log('Started');
+		isRunning = 1;
+		time.setText("0");
+		score.setText("0");
+	}
+
+	// reset scene
+	stopButton() {
+		console.log('Stopped');
+		isRunning = 0;	
+		this.scene.restart();
+	}
+
+	// increment score if game is running
+	circleClick(circle) {
+		if(isRunning) {
+			score.setText(parseInt(score.text.toString())+1);
+			circle.destroy();
+		}
+	}
+
+}
+
+// get random hex color above a color threshold
+function get_hex_color() {
+	var r = Math.floor(Math.max(Math.random()*Math.pow(16, 2), 60)).toString(16);
+	var g = Math.floor(Math.max(Math.random()*Math.pow(16, 2), 60)).toString(16);
+	var b = Math.floor(Math.max(Math.random()*Math.pow(16, 2), 60)).toString(16);
+	
+	if(r.length==1)r="0"+r;
+	if(g.length==1)g="0"+g;
+	if(b.length==1)b="0"+b;
+	
+	return"0x"+r+g+b;
 }
 
 const game = new Phaser.Game({
@@ -60,5 +120,13 @@ const game = new Phaser.Game({
     width: 800,
     height: 600,
     scene: MyScene,
-    physics: { default: 'arcade' },
+    physics: { default: 'arcade'},
     });
+
+
+
+
+
+
+
+
