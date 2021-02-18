c78251f17b7747948bfacd3cae7aa35062d944d9
RCS97 <63381610+RCS97@users.noreply.github.com>
2021-02-18 00:01:49 -0500

    D2 update
    Minor updates to digital assignment 2

--- template-minimal-D2/js/main.js
+++ template-minimal-D2/js/main.js
@@ -16,7 +16,8 @@ var arrow;
 var soccer;
 var net;
 var netWalls;
-
+var instr1;
+var instr2;
 
 
 class MyScene extends Phaser.Scene {
@@ -26,11 +27,11 @@ class MyScene extends Phaser.Scene {
     }
     
     preload() {
+		// images
 		this.load.image( 'background', 'assets/field.jpg' );
 		this.load.image( 'soccer', 'assets/soccer.png' );
 		this.load.image( 'arrow', 'assets/arrow.png' );
 		this.load.image( 'net', 'assets/net.png' );
-		//this.load.image( 'line', 'assets/line2.png' );
 		this.load.image( 'person', 'assets/person.png' );
     }
     
@@ -57,17 +58,17 @@ class MyScene extends Phaser.Scene {
 		time = this.add.text(250, 560, "0", textStyle);
 		score = this.add.text(350, 560, "0", textStyle);
 		
+		instr1 = this.add.text(20, 10, "Left/Right to aim", textStyle);
+		instr2 = this.add.text(20, 40, "Space to kick", textStyle);
 		
 		// objects
-		
 		arrow = this.physics.add.image(400, 500, 'arrow');
 		arrow.setScale(0.2);
-		//arrow.rotation += -1*3.14/2;
 		arrow.setAngle(-90);
 		arrow.setOrigin(0, 0.5);
 		
 		soccer = this.physics.add.sprite(400, 500, 'soccer');
-		soccer.setScale(0.04);
+		soccer.setScale(0.03);
 		
 		net = this.physics.add.sprite(400, 0, 'net');
 		net.setScale(0.1);
@@ -80,10 +81,10 @@ class MyScene extends Phaser.Scene {
 		
 		let p1 = this.physics.add.sprite(400, 150, 'person');
 		let p2 = this.physics.add.sprite(400, 300, 'person');
-		p1.setScale(0.4);
-		p2.setScale(0.4);
-		p1.body.velocity.x = 350;
-		p2.body.velocity.x = -250;
+		p1.setScale(0.3);
+		p2.setScale(0.3);
+		p1.body.velocity.x = 500;
+		p2.body.velocity.x = -400;
 		p1.body.immovable = true;
 		p2.body.immovable = true;
 		p1.setAngle(180);
@@ -115,11 +116,11 @@ class MyScene extends Phaser.Scene {
 				
 				if (this.cursors.left.isDown) {
 					//console.log(arrow.angle);
-					arrow.setAngle(arrow.angle-5);
+					arrow.setAngle(arrow.angle-3);
 				}
 				else if (this.cursors.right.isDown) {
 					//console.log(arrow.angle);
-					arrow.setAngle(arrow.angle+5);
+					arrow.setAngle(arrow.angle+3);
 				}
 				
 				if(this.spaceKey.isDown) {
@@ -136,8 +137,8 @@ class MyScene extends Phaser.Scene {
 
 	// reset scene, then start timer and set to running
 	startButton() {
+		console.log("Game started");
 		this.scene.restart();
-		console.log('Started');
 		isRunning = 1;
 		time.setText("0");
 		score.setText("0");
@@ -150,25 +151,20 @@ class MyScene extends Phaser.Scene {
 		this.scene.restart();
 	}
 
-	// increment score if game is running, remove atom/group
-	circleClick(elemGroup) {
-		if(isRunning) {
-			score.setText(parseInt(score.text.toString())+1);
-			elemGroup.clear(true, true);
-		}
-	}
-
 }
 
+// set velocity
 function kickBall() {
 	console.log("Ball kicked");
-	let v = Math.max(200, Math.sqrt(Math.pow(soccer.body.velocity.x,2)+
+	let v0 = 150;
+	let v = Math.max(v0, Math.sqrt(Math.pow(soccer.body.velocity.x,2)+
 		Math.pow(soccer.body.velocity.y,2))+5);
 	let ang = arrow.angle*Math.PI/180;
 	soccer.body.velocity.x = Math.cos(ang)*v;
 	soccer.body.velocity.y = Math.sin(ang)*v;
 }
 
+// reset ball, increment score
 function goalScored(soccer, net) {
 	console.log("Ball hit net");
 	soccer.body.velocity.x = 0;
@@ -178,30 +174,21 @@ function goalScored(soccer, net) {
 	score.setText(parseInt(score.text.toString())+1);
 }
 
+// test
 function barHit() {
 	console.log("Ball hit crossbar");
 }
 
+// reset ball
 function playerHit() {
 	console.log("Ball hit crossbar");
-	soccer.setX(400);
-	soccer.setY(500);
 	soccer.body.velocity.x = 0;
 	soccer.body.velocity.y = 0;
+	soccer.setX(400);
+	soccer.setY(500);
 }
 
-// get random hex color above a color threshold
-function get_hex_color() {
-	var r = Math.floor(Math.max(Math.random()*Math.pow(16, 2), 60)).toString(16);
-	var g = Math.floor(Math.max(Math.random()*Math.pow(16, 2), 60)).toString(16);
-	var b = Math.floor(Math.max(Math.random()*Math.pow(16, 2), 60)).toString(16);
-	
-	if(r.length==1)r="0"+r;
-	if(g.length==1)g="0"+g;
-	if(b.length==1)b="0"+b;
-	
-	return"0x"+r+g+b;
-}
+
 
 const game = new Phaser.Game({
     type: Phaser.AUTO,
